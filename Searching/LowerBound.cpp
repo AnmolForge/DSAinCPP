1b3a391cb59e587f639ffc178f02ab8b1f30754d
Anmol Singh <anmol.singhdelhi1@gmail.com>
2026-07-22 14:23:07 +0530

    Add files via upload

---
+++ Searching/LowerBound.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[9]={1,2,4,5,9,15,18,21,23};
+    int n=9;
+    int x=20;   //target
+    //linear search
+    for(int i=0; i<n; i++){
+        if(arr[i]>=x){
+            cout<<arr[i-1]<<" is the lower bound"<<endl;
+            
+            break;
+        }
+    }
+    //binary search
+    int lo=0;
+    int hi=n-1;
+    bool flag=false;
+    while(lo<=hi){
+        int mid=lo+(hi-lo)/2;
+        if(arr[mid]==x){
+            flag=true;
+            cout<<arr[mid-1];
+            break;
+        }
+        else if(arr[mid]<x) lo=mid+1;
+        else hi= mid-1;
+    }
+    
+    if(flag==false) cout<<arr[hi];
+}
